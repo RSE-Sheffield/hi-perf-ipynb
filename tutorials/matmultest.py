0b88a5dfce98d3f323798a6bda31ed22cf411218
W. Furnass <will@thearete.co.uk>
2018-04-08 18:54:39 +0100

    multithreading tutorial: add example of using 'time' and OMP_NUM_THREADS

---
+++ tutorials/matmultest.py
@@ -0,0 +1,7 @@
+import numpy as np                                                                                                                                                                          
+
+sq_mat_side = 5000
+a = np.random.rand(sq_mat_side, sq_mat_side)
+b = np.random.rand(sq_mat_side, sq_mat_side)
+a @ b
+
